769939737cd0415b7261dc9d6a26d775d8bc4840
ColdCalzone <coldhotpocket1@gmail.com>
2020-11-16 01:28:33 -0500

    add dynamic projectile removal area system thing
    When any "PlayerAttack" body leaves the area, dynamically set by the code added here, it will get freed to save resources.

--- src/tilesets/tilemap.gd
+++ src/tilesets/tilemap.gd
@@ -6,17 +6,22 @@ onready var garbage_area_shape = $ItsGarbageDay/Shape
 # yoink! F&F2 source code: https://bitbucket.org/JohnGabrielUK/fire-and-fondness-2/src/master
 func _ready():
 	var map_size : Vector2 = get_map_size()
+	var smallest_position: Vector2 = get_smallest_map_position()
 	var data : Array = []
 	for y in map_size.y:
 		var row : Array = []
 		for x in map_size.x:
-			row.append(get_cell(x, y))
+			row.append(get_cellv(Vector2(x, y) + smallest_position))
 		data.append(row)
 	#print(data)
 	OS.clipboard = String(data)
 	garbage_area.connect("body_exited", self, "take_out_the_trash")
-	garbage_area.position = map_to_world(get_map_size()/2)
-	garbage_area_shape.shape.extents = (get_map_size()/2)*64
+	#print(smallest_position.y)
+	# math makes you lose control
+	# math makes you lose control
+	# math makes you lose control
+	garbage_area.position = map_to_world(Vector2(map_size.x + smallest_position.x, map_size.y + smallest_position.y) / 2)
+	garbage_area_shape.shape.extents = ((map_size - smallest_position) / 2) * 64
 	
 func get_map_size() -> Vector2: 
 	var result : Vector2 = Vector2.ZERO
@@ -26,8 +31,13 @@ func get_map_size() -> Vector2:
 			continue
 		result.x = max(result.x, tile.x+1)
 		result.y = max(result.y, tile.y+1)
-	print(result)
+	#print(result)
 	return result
+
+func get_smallest_map_position() -> Vector2:
+	# science
+	#print(get_used_cells()[0])
+	return get_used_cells()[0]
 
 func get_current_friction(pos: Vector2):
 	var map_pos = world_to_map(pos)
