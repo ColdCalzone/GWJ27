57250ad4b41af727d26f5cfb278d64d966205170
ColdCalzone <coldhotpocket1@gmail.com>
2020-11-19 13:09:02 -0500

    Changed stats for enemies, made textbox get out of your face
    :)

--- src/scenes/EnemySpawner.gd
+++ src/scenes/EnemySpawner.gd
@@ -4,19 +4,27 @@ onready var board = get_tree().get_root().get_node("Ground")
 onready var path_container = get_parent().get_node("EnemyPath")
 export var path: Array #= [Vector2(672, -416), Vector2(352, -416), Vector2(352, -352), Vector2(160, -352), Vector2(160, -32), Vector2(352, -32), Vector2(352, 416), Vector2(-96, 416), Vector2(-96, 96), Vector2(160, 96), Vector2(160, 224), Vector2(96, 224)]
 
-var enemy = preload("res://src/scenes/Enemy.tscn")
+var enemies: Dictionary = {
+	"Slime" : preload("res://src/scenes/enemy_scenes/slime.tscn"),
+	"Orange_Slime" : preload("res://src/scenes/enemy_scenes/slime_orange.tscn"),
+	"Stone_Golem" : preload("res://src/scenes/enemy_scenes/Stone_Golem.tscn")
+}
 
 func _ready():
 	for j in path_container.get_children():
 		path.append(board.map_to_world(board.world_to_map(j.position)) + Vector2(32, 32))
-	print(path)
+	#print(path)
 	for i in path:
 		path_container.curve.add_point(i)
-	var enemy_inst = enemy.instance()
-	enemy_inst.position = path[0]
-	enemy_inst.max_health = 7_500_000
-	enemy_inst.damage = 100
-	add_child(enemy_inst)
+		
+	var instances: Dictionary = {
+	"Slime" : enemies.Slime.instance(),
+	"Orange_Slime" : enemies.Orange_Slime.instance(),
+	"Stone_Golem" : enemies.Stone_Golem.instance()
+	}
+	for j in instances.values():
+		j.position = path[0]
+		add_child(j)
 	for i in get_children():
 		i.points = path_container.curve.get_baked_points()
 
